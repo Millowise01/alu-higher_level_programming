092acadbac128550cb80a6a9e49c8de6f4b6a80c
Millowise01 <l.carew@alustudent.com>
2024-10-04 10:04:28 +0000

    read file

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+t fs = require('fs');
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
